051a4acb07a50d1aa18702c87c68eda37da6e734
Mark Morgan <mark.morgan@district5.co.uk>
2024-04-29 07:14:11 +0100

    added more tests

--- tests/FormatterTests/FormatterTest.php
+++ tests/FormatterTests/FormatterTest.php
@@ -23,5 +23,42 @@ class FormatterTest extends TestCase
         $this->assertCount(2, $formatted);
         $this->assertTrue(isset($formatted['first_name']));
         $this->assertTrue(isset($formatted['last_name']));
+        $this->assertEquals($this->basicDTOMock->firstName, $formatted['first_name']);
+        $this->assertEquals($this->basicDTOMock->lastName, $formatted['last_name']);
+    }
+
+    public function testOptionalReturn()
+    {
+        $formatted = BasicFormatterMock::formatSingle(
+            $this->basicDTOMock,
+            [
+                'showTitle' => true
+            ]
+        );
+
+        $this->assertCount(3, $formatted);
+        $this->assertTrue(isset($formatted['first_name']));
+        $this->assertTrue(isset($formatted['last_name']));
+        $this->assertTrue(isset($formatted['title']));
+        $this->assertEquals($this->basicDTOMock->firstName, $formatted['first_name']);
+        $this->assertEquals($this->basicDTOMock->lastName, $formatted['last_name']);
+        $this->assertEquals($this->basicDTOMock->title, $formatted['title']);
+    }
+
+    public function testRejectedReturn()
+    {
+        $formatted = BasicFormatterMock::formatSingle(
+            $this->basicDTOMock,
+            [
+                'showTitle' => false
+            ]
+        );
+
+        $this->assertCount(2, $formatted);
+        $this->assertTrue(isset($formatted['first_name']));
+        $this->assertTrue(isset($formatted['last_name']));
+        $this->assertFalse(isset($formatted['title']));
+        $this->assertEquals($this->basicDTOMock->firstName, $formatted['first_name']);
+        $this->assertEquals($this->basicDTOMock->lastName, $formatted['last_name']);
     }
 }
